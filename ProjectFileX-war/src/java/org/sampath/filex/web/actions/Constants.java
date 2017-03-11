1dcad4370970d16cb17009f78bdaaeff0e07df80
Ashantha Lahiru <ashantha.lahiru@gmail.com>
2017-03-11 08:35:05 +0530

    Resolved Conflictd

--- ProjectFileX-war/src/java/org/sampath/filex/web/actions/Constants.java
+++ ProjectFileX-war/src/java/org/sampath/filex/web/actions/Constants.java
@@ -17,7 +17,7 @@ public class Constants {
     public static final String DB_USERNAME = "hr";
     public static final String DB_PASSWORD = "hr";
     
-    
+//    
 //    public static final String DB_URL = "jdbc:oracle:thin:@192.248.12.15:1521:xe";
 //    public static final String DB_USERNAME = "sampathfilex";
 //    public static final String DB_PASSWORD = "teamx";
