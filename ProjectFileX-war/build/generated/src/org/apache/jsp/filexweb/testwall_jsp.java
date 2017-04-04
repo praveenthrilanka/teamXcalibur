e7f78e33367d0b70b95e98a02069d83249b9f9d3
Praveen Thrilanka <praveenthrilanka@gmail.com>
2017-04-04 19:37:23 +0530

    Edited Commenting System

---
+++ ProjectFileX-war/build/generated/src/org/apache/jsp/filexweb/testwall_jsp.java
@@ -0,0 +1,275 @@
+package org.apache.jsp.filexweb;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+import org.sampath.filex.web.actions.SRS;
+import java.util.ArrayList;
+import org.sampath.filex.web.actions.Comment;
+
+public final class testwall_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html;charset=UTF-8");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("<!DOCTYPE html>\r\n");
+      out.write("<html>\r\n");
+      out.write("    <head>\r\n");
+      out.write("        <!-- \r\n");
+      out.write("         * @Package: Ultra Admin - Responsive Theme\r\n");
+      out.write("         * @Subpackage: Bootstrap\r\n");
+      out.write("         * @Version: 2.0\r\n");
+      out.write("         * This file is part of Ultra Admin Theme.\r\n");
+      out.write("        -->\r\n");
+      out.write("        <meta http-equiv=\"content-type\" content=\"text/html;charset=UTF-8\">\r\n");
+      out.write("        <meta charset=\"utf-8\">\r\n");
+      out.write("        <title>Ultra Admin : Dashboard</title>\r\n");
+      out.write("        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no\">\r\n");
+      out.write("        <meta content=\"\" name=\"description\">\r\n");
+      out.write("        <meta content=\"\" name=\"author\">\r\n");
+      out.write("\r\n");
+      out.write("        <link rel=\"shortcut icon\" href=\"assets/images/favicon.png\" type=\"image/x-icon\">    <!-- Favicon -->\r\n");
+      out.write("        <link rel=\"apple-touch-icon-precomposed\" href=\"assets/images/apple-touch-icon-57-precomposed.png\">\t<!-- For iPhone -->\r\n");
+      out.write("        <link rel=\"apple-touch-icon-precomposed\" sizes=\"114x114\" href=\"assets/images/apple-touch-icon-114-precomposed.png\">    <!-- For iPhone 4 Retina display -->\r\n");
+      out.write("        <link rel=\"apple-touch-icon-precomposed\" sizes=\"72x72\" href=\"assets/images/apple-touch-icon-72-precomposed.png\">    <!-- For iPad -->\r\n");
+      out.write("        <link rel=\"apple-touch-icon-precomposed\" sizes=\"144x144\" href=\"assets/images/apple-touch-icon-144-precomposed.png\">    <!-- For iPad Retina display -->\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("        <!-- CORE CSS FRAMEWORK - START -->\r\n");
+      out.write("        <link href=\"assets/plugins/pace/pace-theme-flash.css\" rel=\"stylesheet\" type=\"text/css\" media=\"screen\">\r\n");
+      out.write("        <link href=\"css/bootstrap.min.css\" rel=\"stylesheet\" type=\"text/css\">\r\n");
+      out.write("        <link href=\"css/bootstrap-theme.min.css\" rel=\"stylesheet\" type=\"text/css\">\r\n");
+      out.write("        <link href=\"fonts/font-awesome/css/font-awesome.css\" rel=\"stylesheet\" type=\"text/css\">\r\n");
+      out.write("        <link href=\"css/animate.min.css\" rel=\"stylesheet\" type=\"text/css\">\r\n");
+      out.write("        <link href=\"plugins/perfect-scrollbar/perfect-scrollbar.css\" rel=\"stylesheet\" type=\"text/css\">\r\n");
+      out.write("        <!-- CORE CSS FRAMEWORK - END -->\r\n");
+      out.write("\r\n");
+      out.write("        <!-- OTHER SCRIPTS INCLUDED ON THIS PAGE - START --> \r\n");
+      out.write("        <link href=\"assets/plugins/morris-chart/css/morris.css\" rel=\"stylesheet\" type=\"text/css\" media=\"screen\"><link href=\"assets/plugins/jquery-ui/smoothness/jquery-ui.min.css\" rel=\"stylesheet\" type=\"text/css\" media=\"screen\"><link href=\"assets/plugins/rickshaw-chart/css/graph.css\" rel=\"stylesheet\" type=\"text/css\" media=\"screen\"><link href=\"assets/plugins/rickshaw-chart/css/detail.css\" rel=\"stylesheet\" type=\"text/css\" media=\"screen\"><link href=\"assets/plugins/rickshaw-chart/css/legend.css\" rel=\"stylesheet\" type=\"text/css\" media=\"screen\"><link href=\"assets/plugins/rickshaw-chart/css/extensions.css\" rel=\"stylesheet\" type=\"text/css\" media=\"screen\"><link href=\"assets/plugins/rickshaw-chart/css/rickshaw.min.css\" rel=\"stylesheet\" type=\"text/css\" media=\"screen\"><link href=\"assets/plugins/rickshaw-chart/css/lines.css\" rel=\"stylesheet\" type=\"text/css\" media=\"screen\"><link href=\"assets/plugins/jvectormap/jquery-jvectormap-2.0.1.css\" rel=\"stylesheet\" type=\"text/css\" media=\"screen\"><link href=\"assets/plugins/icheck/skins/minimal/white.css\" rel=\"stylesheet\" type=\"text/css\" media=\"screen\">        <!-- OTHER SCRIPTS INCLUDED ON THIS PAGE - END --> \r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("        <!-- CORE CSS TEMPLATE - START -->\r\n");
+      out.write("        <link href=\"css/style.css\" rel=\"stylesheet\" type=\"text/css\">\r\n");
+      out.write("        <link href=\"css/responsive.css\" rel=\"stylesheet\" type=\"text/css\">\r\n");
+      out.write("        <!-- CORE CSS TEMPLATE - END -->\r\n");
+      out.write("\r\n");
+      out.write("        <style type=\"text/css\">.jqstooltip { position: absolute;left: 0px;top: 0px;visibility: hidden;background: rgb(0, 0, 0) transparent;background-color: rgba(0,0,0,0.6);filter:progid:DXImageTransform.Microsoft.gradient(startColorstr=#99000000, endColorstr=#99000000);-ms-filter: \"progid:DXImageTransform.Microsoft.gradient(startColorstr=#99000000, endColorstr=#99000000)\";color: white;font: 10px arial, san serif;text-align: left;white-space: nowrap;padding: 5px;border: 1px solid white;z-index: 10000;}.jqsfield { color: white;font: 10px arial, san serif;text-align: left;}</style></head>\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("    <body onload=\"\">\r\n");
+      out.write("        <div class=\"col-md-5 col-sm-12 col-xs-12\">\r\n");
+      out.write("            <div class=\"r3_notification db_box\">\r\n");
+      out.write("                <h4>Notifications</h4>\r\n");
+      out.write("\r\n");
+      out.write("                <ul class=\"list-unstyled notification-widget ps-container ps-active-y\" style=\"height: 315px;\">\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("                    <li class=\"unread status-available\">\r\n");
+      out.write("                        <a href=\"javascript:;\">\r\n");
+      out.write("                            <div class=\"user-img\">\r\n");
+      out.write("                                <img src=\"data/profile/avatar-1.png\" alt=\"user-image\" class=\"img-circle img-inline\">\r\n");
+      out.write("                            </div>\r\n");
+      out.write("                            <div>\r\n");
+      out.write("                                <span class=\"name\">\r\n");
+      out.write("                                    <strong>Clarine Vassar</strong>\r\n");
+      out.write("                                    <span class=\"time small\">- 15 mins ago</span>\r\n");
+      out.write("                                    <span class=\"profile-status available pull-right\"></span>\r\n");
+      out.write("                                </span>\r\n");
+      out.write("                                <span class=\"desc small\">\r\n");
+      out.write("                                    Sometimes it takes a lifetime to win a battle.\r\n");
+      out.write("                                </span>\r\n");
+      out.write("                            </div>\r\n");
+      out.write("                        </a>\r\n");
+      out.write("                    </li>\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("                    <li class=\" status-away\">\r\n");
+      out.write("                        <a href=\"javascript:;\">\r\n");
+      out.write("                            <div class=\"user-img\">\r\n");
+      out.write("                                <img src=\"data/profile/avatar-2.png\" alt=\"user-image\" class=\"img-circle img-inline\">\r\n");
+      out.write("                            </div>\r\n");
+      out.write("                            <div>\r\n");
+      out.write("                                <span class=\"name\">\r\n");
+      out.write("                                    <strong>Brooks Latshaw</strong>\r\n");
+      out.write("                                    <span class=\"time small\">- 45 mins ago</span>\r\n");
+      out.write("                                    <span class=\"profile-status away pull-right\"></span>\r\n");
+      out.write("                                </span>\r\n");
+      out.write("                                <span class=\"desc small\">\r\n");
+      out.write("                                    Sometimes it takes a lifetime to win a battle.\r\n");
+      out.write("                                </span>\r\n");
+      out.write("                            </div>\r\n");
+      out.write("                        </a>\r\n");
+      out.write("                    </li>\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("                    <li class=\" status-busy\">\r\n");
+      out.write("                        <a href=\"javascript:;\">\r\n");
+      out.write("                            <div class=\"user-img\">\r\n");
+      out.write("                                <img src=\"data/profile/avatar-3.png\" alt=\"user-image\" class=\"img-circle img-inline\">\r\n");
+      out.write("                            </div>\r\n");
+      out.write("                            <div>\r\n");
+      out.write("                                <span class=\"name\">\r\n");
+      out.write("                                    <strong>Clementina Brodeur</strong>\r\n");
+      out.write("                                    <span class=\"time small\">- 1 hour ago</span>\r\n");
+      out.write("                                    <span class=\"profile-status busy pull-right\"></span>\r\n");
+      out.write("                                </span>\r\n");
+      out.write("                                <span class=\"desc small\">\r\n");
+      out.write("                                    Sometimes it takes a lifetime to win a battle.\r\n");
+      out.write("                                </span>\r\n");
+      out.write("                            </div>\r\n");
+      out.write("                        </a>\r\n");
+      out.write("                    </li>\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("                    <li class=\" status-offline\">\r\n");
+      out.write("                        <a href=\"javascript:;\">\r\n");
+      out.write("                            <div class=\"user-img\">\r\n");
+      out.write("                                <img src=\"data/profile/avatar-4.png\" alt=\"user-image\" class=\"img-circle img-inline\">\r\n");
+      out.write("                            </div>\r\n");
+      out.write("                            <div>\r\n");
+      out.write("                                <span class=\"name\">\r\n");
+      out.write("                                    <strong>Carri Busey</strong>\r\n");
+      out.write("                                    <span class=\"time small\">- 5 hours ago</span>\r\n");
+      out.write("                                    <span class=\"profile-status offline pull-right\"></span>\r\n");
+      out.write("                                </span>\r\n");
+      out.write("                                <span class=\"desc small\">\r\n");
+      out.write("                                    Sometimes it takes a lifetime to win a battle.\r\n");
+      out.write("                                </span>\r\n");
+      out.write("                            </div>\r\n");
+      out.write("                        </a>\r\n");
+      out.write("                    </li>\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("                    <li class=\" status-offline\">\r\n");
+      out.write("                        <a href=\"javascript:;\">\r\n");
+      out.write("                            <div class=\"user-img\">\r\n");
+      out.write("                                <img src=\"data/profile/avatar-5.png\" alt=\"user-image\" class=\"img-circle img-inline\">\r\n");
+      out.write("                            </div>\r\n");
+      out.write("                            <div>\r\n");
+      out.write("                                <span class=\"name\">\r\n");
+      out.write("                                    <strong>Melissa Dock</strong>\r\n");
+      out.write("                                    <span class=\"time small\">- Yesterday</span>\r\n");
+      out.write("                                    <span class=\"profile-status offline pull-right\"></span>\r\n");
+      out.write("                                </span>\r\n");
+      out.write("                                <span class=\"desc small\">\r\n");
+      out.write("                                    Sometimes it takes a lifetime to win a battle.\r\n");
+      out.write("                                </span>\r\n");
+      out.write("                            </div>\r\n");
+      out.write("                        </a>\r\n");
+      out.write("                    </li>\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("                    <li class=\" status-available\">\r\n");
+      out.write("                        <a href=\"javascript:;\">\r\n");
+      out.write("                            <div class=\"user-img\">\r\n");
+      out.write("                                <img src=\"data/profile/avatar-1.png\" alt=\"user-image\" class=\"img-circle img-inline\">\r\n");
+      out.write("                            </div>\r\n");
+      out.write("                            <div>\r\n");
+      out.write("                                <span class=\"name\">\r\n");
+      out.write("                                    <strong>Verdell Rea</strong>\r\n");
+      out.write("                                    <span class=\"time small\">- 14th Mar</span>\r\n");
+      out.write("                                    <span class=\"profile-status available pull-right\"></span>\r\n");
+      out.write("                                </span>\r\n");
+      out.write("                                <span class=\"desc small\">\r\n");
+      out.write("                                    Sometimes it takes a lifetime to win a battle.\r\n");
+      out.write("                                </span>\r\n");
+      out.write("                            </div>\r\n");
+      out.write("                        </a>\r\n");
+      out.write("                    </li>\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("                    <li class=\" status-busy\">\r\n");
+      out.write("                        <a href=\"javascript:;\">\r\n");
+      out.write("                            <div class=\"user-img\">\r\n");
+      out.write("                                <img src=\"data/profile/avatar-2.png\" alt=\"user-image\" class=\"img-circle img-inline\">\r\n");
+      out.write("                            </div>\r\n");
+      out.write("                            <div>\r\n");
+      out.write("                                <span class=\"name\">\r\n");
+      out.write("                                    <strong>Linette Lheureux</strong>\r\n");
+      out.write("                                    <span class=\"time small\">- 16th Mar</span>\r\n");
+      out.write("                                    <span class=\"profile-status busy pull-right\"></span>\r\n");
+      out.write("                                </span>\r\n");
+      out.write("                                <span class=\"desc small\">\r\n");
+      out.write("                                    Sometimes it takes a lifetime to win a battle.\r\n");
+      out.write("                                </span>\r\n");
+      out.write("                            </div>\r\n");
+      out.write("                        </a>\r\n");
+      out.write("                    </li>\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("                    <li class=\" status-away\">\r\n");
+      out.write("                        <a href=\"javascript:;\">\r\n");
+      out.write("                            <div class=\"user-img\">\r\n");
+      out.write("                                <img src=\"data/profile/avatar-3.png\" alt=\"user-image\" class=\"img-circle img-inline\">\r\n");
+      out.write("                            </div>\r\n");
+      out.write("                            <div>\r\n");
+      out.write("                                <span class=\"name\">\r\n");
+      out.write("                                    <strong>Araceli Boatright</strong>\r\n");
+      out.write("                                    <span class=\"time small\">- 16th Mar</span>\r\n");
+      out.write("                                    <span class=\"profile-status away pull-right\"></span>\r\n");
+      out.write("                                </span>\r\n");
+      out.write("                                <span class=\"desc small\">\r\n");
+      out.write("                                    Sometimes it takes a lifetime to win a battle.\r\n");
+      out.write("                                </span>\r\n");
+      out.write("                            </div>\r\n");
+      out.write("                        </a>\r\n");
+      out.write("                    </li>\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("                    <div class=\"ps-scrollbar-x-rail\" style=\"left: 0px; bottom: 3px;\"><div class=\"ps-scrollbar-x\" style=\"left: 0px; width: 0px;\"></div></div><div class=\"ps-scrollbar-y-rail\" style=\"top: 0px; height: 315px; right: 3px;\"><div class=\"ps-scrollbar-y\" style=\"top: 0px; height: 134px;\"></div></div></ul>\r\n");
+      out.write("\r\n");
+      out.write("            </div>\r\n");
+      out.write("        </div>\r\n");
+      out.write("    </body>\r\n");
+      out.write("</html>\r\n");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
